b28dc1721cf4d977bd010bb8fd00185b368663ce
jjf7 <jfsistemas.contacto@gmail.com>
2022-06-25 11:35:28 -0400

    wss again =V

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const httpLink = createHttpLink({
 });
 
 const wsLink = new WebSocketLink({
-  uri: "WSS://apollo-server-jfdesousa.herokuapp.com/graphql",
+  uri: "wss://apollo-server-jfdesousa.herokuapp.com/graphql",
   options: {
     reconnect: true,
   },
